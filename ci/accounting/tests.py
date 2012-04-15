b792e4f918a2c7f4d561b7ff2e686c1a42e2d85a
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-04-16 02:06:53 +0400

    small code formatting style update: tabs to 4 spaces, according to PEP

--- ci/accounting/tests.py
+++ ci/accounting/tests.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
 from ci.accounting.models import Account
 
-
 class TestAccountTransaction(TestCase):
     def test_transaction_succed(self):
         """
-	Tests valid transaction succed
+        Tests valid transaction succed
         """
-	account1 = Account.objects.create(amount=25)
-	account2 = Account.objects.create(amount=0)
-	account1.withdraw(10, account2)
-	self.assertEquals(account1.amount, 15)
-	self.assertEquals(account2.amount, 10)
+        account1 = Account.objects.create(amount=25)
+        account2 = Account.objects.create(amount=0)
+        account1.withdraw(10, account2)
+        self.assertEquals(account1.amount, 15)
+        self.assertEquals(account2.amount, 10)
