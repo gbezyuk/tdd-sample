82cd62e00c26fdb402c84a7ab4b305f62cbe5e5f
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-04-16 02:24:33 +0400

    one more test; django_any in action

--- ci/accounting/models.py
+++ ci/accounting/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Account(models.Model):
     amount = models.IntegerField()
+    user = models.ForeignKey(to=User)
 
     def withdraw(self, amount, account):
+        assert self.amount >= amount
         self.amount -= amount
         account.amount += amount
--- ci/accounting/tests.py
+++ ci/accounting/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
 from ci.accounting.models import Account
+from django_any import any_model
 
 class TestAccountTransaction(TestCase):
     def test_transaction_succed(self):
         """
         Tests valid transaction succed
         """
-        account1 = Account.objects.create(amount=25)
-        account2 = Account.objects.create(amount=0)
+        account1 = any_model(Account, amount=25)
+        account2 = any_model(Account, amount=0)
         account1.withdraw(10, account2)
         self.assertEquals(account1.amount, 15)
         self.assertEquals(account2.amount, 10)
+    def test_transaction_fails(self):
+        """
+        Tests invalid transaction fail (insufficient money amount)
+        """
+        account1 = any_model(Account, amount=10)
+        account2 = any_model(Account)
+        self.assertRaises(AssertionError, account1.withdraw, 100, account2)
