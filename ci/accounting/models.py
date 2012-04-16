8fc2d89e1160887b672a9ce8f4b7d6b22183c12d
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-04-16 12:05:49 +0400

    Currency entity. Two more tests

--- ci/accounting/models.py
+++ ci/accounting/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Currency(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
 class Account(models.Model):
     amount = models.IntegerField()
     user = models.ForeignKey(to=User)
+    currency = models.ForeignKey(to=Currency)
 
-    def withdraw(self, amount, account):
+    def withdraw(self, amount, target_account):
         assert self.amount >= amount
+        assert self.currency.name == target_account.currency.name
         self.amount -= amount
-        account.amount += amount
+        target_account.amount += amount
--- ci/accounting/tests.py
+++ ci/accounting/tests.py
@@ -1,21 +1,42 @@
 from django.test import TestCase
-from ci.accounting.models import Account
+from ci.accounting.models import Account, Currency
 from django_any import any_model
+from django.core.exceptions import ValidationError
 
 class TestAccountTransaction(TestCase):
+    def setUp(self):
+        """
+        Setting up model instances. Using factory (django_any) instead of fixtures (couse fixtures are evil)
+        """
+        self.currency1 = any_model(Currency)    
+        self.currency2 = any_model(Currency)
+        self.account1 = any_model(Account, amount=25, currency=self.currency1)
+        self.account2 = any_model(Account, amount=0, currency=self.currency1)
+        self.account3 = any_model(Account, amount=1000)
+
+    def test_currency_create_same_name_fails(self):
+        """
+        Tests that creation of two currencies with same names will fail
+        """
+        c1 = any_model(Currency)
+        self.assertRaises(ValidationError, any_model, Currency, **{'name': c1.name})
+
     def test_transaction_succed(self):
         """
         Tests valid transaction succed
         """
-        account1 = any_model(Account, amount=25)
-        account2 = any_model(Account, amount=0)
-        account1.withdraw(10, account2)
-        self.assertEquals(account1.amount, 15)
-        self.assertEquals(account2.amount, 10)
+        self.account1.withdraw(10, self.account2)
+        self.assertEquals(self.account1.amount, 15)
+        self.assertEquals(self.account2.amount, 10)
+
     def test_transaction_fails(self):
         """
         Tests invalid transaction fail (insufficient money amount)
         """
-        account1 = any_model(Account, amount=10)
-        account2 = any_model(Account)
-        self.assertRaises(AssertionError, account1.withdraw, 100, account2)
+        self.assertRaises(AssertionError, self.account1.withdraw, 100, self.account2)
+
+    def test_different_currencies_transaction_fails(self):
+        """
+        Tests that transaction with accounts having different currencies fails
+        """
+        self.assertRaises(AssertionError, self.account3.withdraw, 100, self.account2)
